6699ff2ab7b80e3688d3c89584089c9cdd3b220e
Maylene Poulsen <mayleneski@hotmail.com>
2019-12-21 16:22:34 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -59,7 +59,12 @@ end
 
 
 def reduce_to_all_true(source_array)
-  source_array.all?
+  i = 0
+  while i < source_array.length do
+    return false if !source_array[i]
+    i += 1
+  end
+  return true
 end  
  
 
